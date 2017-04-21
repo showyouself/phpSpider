2582ab256006786f80ef94864c453e4fbeb7325d
zengbin <200854748@qq.com>
2017-04-22 03:37:26 +0800

    modify times

--- crontab/second.sh
+++ crontab/second.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-for loop in {1..2}
+for loop in {1..7}
 do
-	for loop in {1..10}
+	for loop in {1..50}
 	do
+		sleep 0.15
 		step=`tail /home/ben/work/swoole/spider/crontab/step.lock -n1`
 		step=`expr "${step}" + "1"`
 		`echo ${step} >> /home/ben/work/swoole/spider/crontab/step.lock`
 		curl "127.0.0.1:9501?id=${step}"
 	done
+	sleep 5;
 done
