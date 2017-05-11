07fa731b33c3548e59ff909751aecfba63bf0e1e
zengbin <200854748@qq.com>
2017-05-11 15:26:46 +0800

    update admin

--- bin/admin.sh
+++ bin/admin.sh
@@ -2,14 +2,13 @@
 #移动到运行命令
 cd /home/ben/work/swoole/spider/bin
 second_log=/home/ben/work/swoole/spider/log/second.log
+process="main.php"
 
 #接受信号
 if [ $1 == "stop" ]
 then
-	`kill -9 $(pidof php main.php)`
-	sleep 2
-	`kill -9 $(pidof php main.php)`
-	`kill -9 $(ps -ef|grep "second"|awk '{print $2}')`
+	`kill -9 $(ps -ef|grep $process|grep -v "grep"|awk '{print $2}')`
+	`kill -9 $(ps -ef|grep "second"|grep -v "grep"|awk '{print $2}')`
 	echo "stop success"
 elif [ $1 == "start" ]
 then	
@@ -18,21 +17,19 @@ then
 	echo "start success"
 elif [ $1 == "restart" ]
 then 
-	`kill -9 $(pidof php main.php)`
-	sleep 2
-	`kill -9 $(pidof php main.php)`
-	`kill -9 $(ps -ef|grep "second"|awk '{print $2}')`
+	`kill -9 $(ps -ef|grep $process|grep -v "grep"|awk '{print $2}')`
+	`kill -9 $(ps -ef|grep "second"|grep -v "grep"|awk '{print $2}')`
 	php main.php
 	nohup ../crontab/second.sh >> $second_log 2>&1 &
 	echo "restart success"
 elif [ $1 == "sh_restart" ]
 then
-	`kill -9 $(ps -ef|grep "second"|awk '{print $2}')`
+	`kill -9 $(ps -ef|grep "second"|grep -v "grep"|awk '{print $2}')`
 	nohup ../crontab/second.sh >> $second_log 2>&1 &
 elif [ $1 == "status" ]
 then 
 	ps -ef|grep "main.php"
-	ps -ef|grep "second"
+	ps -ef|grep "second"|grep -v "grep"
 else
 	echo "usage:<sh_restart|restart|start|stop>"
 fi
